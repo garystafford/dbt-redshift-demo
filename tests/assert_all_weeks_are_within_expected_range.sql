b4964c48ebe4ddb32bae34a4bf3d05a429c0c9a3
Gary A. Stafford <gary.a.stafford@gmail.com>
2022-12-30 19:58:40 -0500

    Fix week value test

--- tests/assert_all_weeks_are_within_expected_range.sql
+++ tests/assert_all_weeks_are_within_expected_range.sql
@@ -1,5 +1,5 @@
--- expected numeric range for week of year: 1-52
-select day
+-- expected numeric range for week of year: 1-53
+select week
 from {{ ref('stg_tickit__dates') }}
-group by day
-having not (day >= 1 and day <= 52)
+group by week
+having not (week >= 1 and week <= 53)
