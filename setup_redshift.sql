c0e53c1fd3f525502698e4b41e666975ddc215a0
Gary A. Stafford <garystafford@users.noreply.github.com>
2023-01-03 16:11:16 -0500

    Create setup_redshift.sql

---
+++ setup_redshift.sql
@@ -0,0 +1,48 @@
+-- redshift setup
+
+-- optional: drop previous demo
+drop database demo;
+
+-- optional: drop previous user
+drop user dbt;
+
+-- optional: drop previous group
+drop group dbt
+
+-- create new database
+create database demo with owner admin;
+
+-- switch connection to new demo database before continuing!
+
+-- create schemas
+create external schema tickit_external
+from data catalog
+database 'tickit_dbt'
+iam_role 'arn:aws:iam::<your_aws_acccount_id>:role/ClusterPermissionsRole'
+create external database if not exists;
+
+create schema tickit_dbt;
+
+drop schema public;
+
+-- create dbt user and group
+create user dbt with password 'CHANGE_ME!'
+    nocreatedb nocreateuser syslog access restricted
+    connection limit 10;
+
+-- create dbt group
+create group dbt with user dbt;
+
+-- grants on tickit_external schema
+grant usage on schema tickit_external to group dbt;
+grant create on schema tickit_external to group dbt;
+grant all on all tables in schema tickit_external to group dbt;
+
+-- grants on tickit_dbt schema
+grant usage on schema tickit_dbt to group dbt;
+grant create on schema tickit_dbt to group dbt;
+grant all on all tables in schema tickit_dbt to group dbt;
+
+-- reassign schema ownership to dbt
+alter schema tickit_dbt owner to dbt;
+alter schema tickit_external owner to dbt;
